f68941b40ab954c0f6c4608bff7b55ba4cd52e83
Gabriel <gabrielvanzella1@hotmail.com>
2024-01-18 13:30:28 -0300

    ajuste nas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\DashboardController;
 
 
-
-Route::controller(HomeController::class)->group(function() {
+Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home.index');
 });
 
-Route::controller(DashboardController::class)->group(function() {
-    Route::middleware([
-        'auth:sanctum',
-        config('jetstream.auth_session'),
-        'verified',
-    ])->group(function () {
-        Route::get('/dashboard', function () {
-            return view('dashboard');
-        })->name('dashboard');
-    });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
 Route::middleware([
